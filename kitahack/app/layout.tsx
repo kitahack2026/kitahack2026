abb3bbffe3ced1861110cbb8b5491d6bccf89831
mteh0004 <146699335+fichera9188@users.noreply.github.com>
2025-12-30 00:39:07 +0800

    feat: update favicon for enhanced branding

--- kitahack/app/layout.tsx
+++ kitahack/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="./public/tabLogo.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
