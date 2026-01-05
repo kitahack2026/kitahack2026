531b583f6fcd8161c88ec0d8255b4e999242220a
Cindy Pua <cindyqi0511@gmail.com>
2026-01-06 01:03:16 +0800

    Revert "Merge branch 'main' of https://github.com/kitahack2026/kitahack2026"
    This reverts commit c635ceab79732a968944ebf39bde1932cdbd405e, reversing
    changes made to d45239ea5914b4367bb55449744daec2b2ea784d.

--- kitahack/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://kitahack2026.my/', // domain
-  generateRobotsTxt: true, // Generate robots.txt automatically
-  changefreq: 'weekly', // Optional: page change frequency
-  priority: 0.7, // Optional: page priority
-  sitemapSize: 5000, // Optional: max URLs per sitemap file
-};
